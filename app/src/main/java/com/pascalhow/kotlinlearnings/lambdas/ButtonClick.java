3200b55cb2de5acc1fb9cb2a1475dc3e521f5c67
James Coggan <james.coggan@mydrivesolutions.com>
2018-04-03 20:41:40 +0100

    Added More button examples

--- app/src/main/java/com/pascalhow/kotlinlearnings/lambdas/ButtonClick.java
+++ app/src/main/java/com/pascalhow/kotlinlearnings/lambdas/ButtonClick.java
@@ -5,13 +5,32 @@ import android.view.View;
 import android.widget.Button;
 
 public class ButtonClick {
-    public ButtonClick(Button button) {
+    public ButtonClick(final Button button) {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("TAG", "ButtonClicked" + v.getId());
+                Log.d("TAG", "ButtonClicked:" + v.getId());
             }
         });
 
+        button.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                String message =
+                        "OnLayoutChange" + v.getId() +
+                        "left:" + left +
+                        "top:" + top +
+                        "right:" + right +
+                        "bottom:" + bottom;
+                Log.d("TAG", message);
+            }
+        });
+
+        button.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                Log.d("TAG", "onFocusChange:" + v.getId() + "hasFocus:" + hasFocus);
+            }
+        });
     }
 }
--- app/src/main/java/com/pascalhow/kotlinlearnings/lambdas/ButtonClickKt.kt
+++ app/src/main/java/com/pascalhow/kotlinlearnings/lambdas/ButtonClickKt.kt
@@ -6,5 +6,9 @@ import android.widget.Button
 class ButtonClickKt(button: Button) {
     init {
         button.setOnClickListener { Log.d("TAG", "ButtonClicked ${it.id}") }
+        button.addOnLayoutChangeListener { v, left, top, right, bottom, _, _, _, _ ->
+            Log.d("TAG", "ButtonClicked ${v.id} left:$left top:$top right:$right bottom:$bottom")
+        }
+        button.setOnFocusChangeListener { v, focus -> Log.d("TAG", "onFocusChange:${v.id} focus:$focus") }
     }
 }
